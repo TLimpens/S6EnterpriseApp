d84acbf559f22bd15d5b6a9f5c547ea91abc04a6
Tom <tom.limpens@hotmail.com>
2020-03-16 16:03:35 +0100

    set up IdentityServer working out clientcredentialflow

---
+++ ResourceServer/ResourceServer/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResourceServer.Controllers
+{
+    public class SecretController : Controller
+    {
+
+        [Route("/secret")]
+        [Authorize]
+        public string Index()
+        {
+            return "Secret message from Resource server";
+        }
+    }
+}
---
+++ Services/Identity/ResourceClient/ResourceClient/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IdentityModel.Client;
+
+namespace ResourceClient.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("/")]
+        public async Task<IActionResult> Index()
+        {
+
+            //retrieve acces token
+            var serverClient = _httpClientFactory.CreateClient();
+
+            var discoveryDocument = await serverClient.GetDiscoveryDocumentAsync("https://localhost:5001/");
+
+
+            var tokenResponse = await serverClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest {
+            
+                Address = discoveryDocument.TokenEndpoint,
+
+                ClientId = "client_id",
+                ClientSecret = "client_secret",
+
+                Scope = "ResourceServer"
+            });
+
+            //retrive secret data
+            var apiClient = _httpClientFactory.CreateClient();
+
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:5003/secret");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return Ok(new {
+            
+                acces_token = tokenResponse.AccessToken,
+                message = content,
+            
+            });
+        }
+    }
+}
